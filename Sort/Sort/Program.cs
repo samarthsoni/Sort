cb6a1dada2df515a407b2d8fe68ecc9d066b4376
Christoph Donges <christoph.donges@epicdigitalau.com>
2016-12-09 11:44:09 +1000

    Tidy up code

--- Sort/Sort/Program.cs
+++ Sort/Sort/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,18 +18,16 @@ namespace Sort
             if (System.IO.File.Exists(path))
             {
                 //Declaring a list of objects of class person to represent different persons
-                List<person> all_persons = new List<person>();
-
                 //Method read_textfile will read the text file and extract all names and score
                 //and will return a populated object list of person
-                all_persons = read_textfile(path, all_persons);
+                List<person> all_persons = read_textfile(path);
 
                 //Method sort_persons will sort list of objects of person in descending order
                 all_persons = sort_persons(all_persons);
 
                 //Method create_path will generate the path of output file with appropriate name
                 string[] newpath = create_path(path);
-                string new_filepath = newpath[0] + "\\" + newpath[1] + "-graded" + newpath[2];
+                string new_filepath = Path.Combine(newpath[0], newpath[1] + "-graded" + newpath[2]);
                 
                 //Method write_textfile will write the output to a text file
                 write_textfile(all_persons, new_filepath);
@@ -44,36 +43,38 @@ namespace Sort
         /* Method read_textfile accepts a filepath and empty list of objects of class person as arguments
          * Reads the text file and populates the list of objects
          * Returns a populated list of objects of class person                                          */
-        public static List<person> read_textfile(string path, List<person> all_persons)
+        public static List<person> read_textfile(string path)
         {
+            List <person> all_persons = new List<person>();
 
             try
             {
-
-                System.IO.TextReader reader = new System.IO.StreamReader(path);
-                string line = "";
-                while ((line = reader.ReadLine()) != null)
+                using (System.IO.TextReader reader = new System.IO.StreamReader(path))
                 {
-                    if (line  == ""||line==" ")
+                    string line;
+                    while ((line = reader.ReadLine()) != null)
                     {
-                        continue;
-                    }
-                    else
-                    {
-                        char[] delims = { ' ', ',', '\n' };
-                        string[] words = line.Split(delims, StringSplitOptions.RemoveEmptyEntries);
-
-                        int i;
-                        if (int.TryParse(words[2], out i) == false)
+                        if (string.IsNullOrWhiteSpace(line))
                         {
-                            Console.WriteLine("Invalid score value");
-                            Environment.Exit(0);
+                            continue;
                         }
-                        all_persons.Add(new person(words[0], words[1], i));
-                    }
+                        else
+                        {
+                            char[] delims = {' ', ',', '\n'};
+                            string[] words = line.Split(delims, StringSplitOptions.RemoveEmptyEntries);
 
+                            int i;
+                            if (int.TryParse(words[2], out i) == false)
+                            {
+                                Console.WriteLine("Invalid score value");
+                                Environment.Exit(0);
+                            }
+                            all_persons.Add(new person(words[0], words[1], i));
+                        }
+
+                    }
+                    reader.Close();
                 }
-                reader.Close();
 
             }
             catch (Exception e)
@@ -94,14 +95,15 @@ namespace Sort
                 {
                     System.IO.File.Delete(new_path);
                 }
-                System.IO.TextWriter writer = new System.IO.StreamWriter(new_path);
-
-                foreach (person p in all)
+                using (System.IO.TextWriter writer = new System.IO.StreamWriter(new_path))
                 {
-                    Console.WriteLine("\n"+p.LastName + ", " + p.FirstName + ", " + p.Score);
-                    writer.WriteLine(p.LastName + ", " + p.FirstName + ", " + p.Score);
+                    foreach (person p in all)
+                    {
+                        Console.WriteLine("\n" + p.LastName + ", " + p.FirstName + ", " + p.Score);
+                        writer.WriteLine(p.LastName + ", " + p.FirstName + ", " + p.Score);
+                    }
+                    writer.Close();
                 }
-                writer.Close();
             }
             catch (Exception e)
             {
--- Sort/SortTest/SortPersonsTests.cs
+++ Sort/SortTest/SortPersonsTests.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Sort;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 
 namespace SortTest
@@ -13,33 +15,18 @@ namespace SortTest
         public void TestMethod_read_textfile()
         {
             //arrange
-            List<Sort.person> test_persons = new List<Sort.person>();
             /*Change the path of the text file which can be used for testing of read_textfile method*/
-            string path = "F:\\workspace\\C# workspace\transmax_question\names2.txt";
+            string path = Path.Combine(AppDomain.CurrentDomain.SetupInformation.ApplicationBase, "names2.txt");
 
-            string[] Last= new string[] { "DALEY", "CALAM", "ROWE" };
-            string[] First = new string[] { "ROSS", "JILL", "CHRIS" };
-            int[] Score1 = new int[] { 84, 62, 77 };
-            List<Sort.person> expected_persons = new List<Sort.person>();
-            int i = 0;
-            foreach(person p in expected_persons)
-            {
-                p.LastName = Last[i];
-                p.FirstName = First[i];
-                p.Score = Score1[i];
-                i++;
-            }
+            List<Sort.person> expected_persons = GetFileList();
 
             //act
-            test_persons = Program.read_textfile(path, test_persons);
-
-
+            List<Sort.person> test_persons = Program.read_textfile(path);
+            
             //Assert
             Assert.AreEqual(expected_persons.Count, test_persons.Count, "Text file incorrectly read");
             if(expected_persons.Count==test_persons.Count)
             {
-                expected_persons.ToArray();
-                test_persons.ToArray();
                 for (int k = 0; k < expected_persons.Count;k++)
                 {
                     Assert.AreEqual(expected_persons[k], test_persons[k], "Text file incorrectly read");
@@ -48,28 +35,56 @@ namespace SortTest
             
         }
 
+        public List<person> GetFileList()
+        {
+            return new List<Sort.person>()
+            {
+                new person("DALEY", "ROSS", 84),
+                new person("DALEY", "BRUNDA", 84),
+                new person("CALAM", "JILL", 62),
+                new person("ROWE", "CHRIS", 77),
+                new person("JAMES", "AMI", 55),
+                new person("STEWART", "GLENN", 82),
+                new person("PERRIN", "DIMITRI", 82),
+            };
+        }
+
+        public List<person> GetTestList()
+        {
+            return new List<Sort.person>()
+            {
+                new person("DALEY", "ROSS", 84),
+                new person("CALAM", "JILL", 62),
+                new person("ROWE", "CHRIS", 77),
+                new person("DALEY", "BRUNDA", 84),
+                new person("JAMES", "AMI", 55),
+                new person("STEWART", "GLENN", 82),
+                new person("PERRIN", "DIMITRI", 82),
+            };
+        }
+
+        public List<person> GetExpectedList()
+        {
+            return new List<Sort.person>()
+            {
+                new person("DALEY", "BRUNDA", 84),
+                new person("DALEY", "ROSS", 84),
+                new person("PERRIN", "DIMITRI", 82),
+                new person("STEWART", "GLENN", 82),
+                new person("ROWE", "CHRIS", 77),
+                new person("CALAM", "JILL", 62),
+                new person("JAMES", "AMI", 55),
+            };
+        }
+
         //unit test for sort_persons method
         [TestMethod]
         public void TestMethod_sort_persons()
         {
             //arrange
-            List<person> test_list = new List<person>();
-            test_list.Add(new person("DALEY", "ROSS", 84));
-            test_list.Add(new person("DALEY", "BRUNDA", 84));
-            test_list.Add(new person("CALAM", "JILL", 62));
-            test_list.Add(new person("ROWE", "CHRIS", 77));
-            test_list.Add(new person("JAMES", "AMI", 55));
-            test_list.Add(new person("STEWART", "GLENN", 82));
-            test_list.Add(new person("PERRIN", "DIMITRI", 82));
+            List<person> test_list = GetTestList();
             
-            List<person> expected_list = new List<person>();
-            expected_list.Add(new person("DALEY", "BRUNDA", 84));
-            expected_list.Add(new person("DALEY", "ROSS", 84));
-            expected_list.Add(new person("PERRIN", "DIMITRI", 82));
-            expected_list.Add(new person("STEWART", "GLENN", 82));
-            expected_list.Add(new person("ROWE", "CHRIS", 77));
-            expected_list.Add(new person("CALAM", "JILL", 62));
-            expected_list.Add(new person("JAMES", "AMI", 55));
+            List<person> expected_list = GetExpectedList();
 
             //act
             List<person> actual_list = new List<person>();
@@ -79,9 +94,6 @@ namespace SortTest
             Assert.AreEqual(expected_list.Count, actual_list.Count, "Names incorrectly sized");
             if (expected_list.Count == actual_list.Count && actual_list.Count!=0 && expected_list.Count!=0)
             {
-
-                expected_list.ToArray();
-                actual_list.ToArray();
                 for (int k = 0; k < expected_list.Count; k++)
                 {
                     Assert.AreEqual(expected_list[k], actual_list[k],"Incorrect Sorting");
